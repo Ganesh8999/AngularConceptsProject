38d6fe30890b741f4f3caf20b43151937af98889
Ganesh8999 <Ganesh8999@github.com>
2020-09-21 12:45:22 +0530

    creating data bound component

---
+++ data-bound/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
